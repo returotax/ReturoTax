623bf83bc481780788668793b2b88c639c7d7d8b
returotax <returotax@gmail.com>
2026-03-30 14:39:51 +0300

    Create index.tsx for the main page

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+// This is the main entry point for the ReturoTax application
+import React from 'react';
+
+const Index = () => {
+    return <div>Welcome to ReturoTax!</div>;
+};
+
+export default Index;
